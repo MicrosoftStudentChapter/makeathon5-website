2298a29a5bb2ef41975ed10a5db21778eca96269
destrex271 <destrex271@gmail.com>
2023-01-22 00:26:40 +0530

    app initialized

---
+++ src/pages/Landing/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import landingStlyes from './landing.module.css';
+import Box from '@mui/material/Box';
+
+const Landing = () => {
+  return(
+    <Box
+      sx={{
+        width: '100vw',
+        height: '100vh',
+        backgroundColor: '#1e1e1e',
+      }}
+    />
+  )
+} 
+
+export default Landing;
