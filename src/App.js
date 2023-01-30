f4f163c4370e23b5df3662a9056ed2d0ad900e4c
destrex271 <destrex271@gmail.com>
2023-01-30 23:02:27 +0530

    navbar fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import Landing from './pages/Landing';
 import './index.css';
 import {Navbar} from './components/Navbar'
+import LocomotiveScroll from 'locomotive-scroll';
+import { useRef } from 'react';
 // import {black} from '@mui/material/colors';
 
 function App() {
+  const ref = useRef(null)
+  let options = {
+    smooth : true
+  }
   return (
     <>
+    {/* <LocomotiveScroll */}
+    {/*   option={options} */}
+    {/* > */}
       <Navbar />
-      <section id="landing">
-        <Landing />
-      </section>
+      <main
+        data-scroll-conatiner
+        ref={ref}
+      >
+        <section 
+          id="landing"
+        
+        >
+          <Landing />
+        </section>
+      </main>
     </>
   );
 }
