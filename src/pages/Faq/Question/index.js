d07095a64fdb6be56101f454087769e23065485b
ikshan-Tango <ikshanbhardwaj.2003@gmail.com>
2023-02-01 01:23:30 +0530

    arre betichod usestate toh boohot asaan hai!

--- src/pages/Faq/Question/index.js
+++ src/pages/Faq/Question/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React ,{useEffect, useState} from "react";
 
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
@@ -10,19 +10,26 @@ import { StylesContext } from "@mui/styles";
 import styles from './question.module.css'
 
 export default function Question({ques, ans}) {
+
+  const [sign,setSign] = useState(true)
+
   return (
     <div className={styles.wrapper}>
       <Accordion sx={{backgroundColor:'#ffffff5A',color:'white', backdropFilter:'blur(10px)'}}>
         <AccordionSummary
+        onClick={() => {setSign(!sign)}}
+        
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
           id="panel1a-header"
           
         >
-          <Typography>{ques}</Typography>
+          <Typography sx={{fontFamily:'Lexend Exa',fontStyle:'normal',fontWeight:'700',fontSize:'1rem'}}>{sign ? "+"  : "-"} {ques}</Typography>
         </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
+        <AccordionDetails
+        onClose = {() => {setSign(true); console.log("ok")}}
+        >
+          <Typography sx={{fontFamily:'Lexend Exa',fontStyle:'normal',fontSize:'1rem'}}>
             {ans}
           </Typography>
         </AccordionDetails>
