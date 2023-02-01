0c1a4f04fc2fd5e25a9215b4d31b696b36d7af6f
Utkarsh <utripathi2002@gmail.com>
2023-02-02 00:48:07 +0530

    loader component added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-import Landing from './pages/Landing';
-import './index.css';
-import './App.css';
-import {Navbar} from './components/Navbar'
-import {Faq} from './pages/Faq'
-
-import { useRef } from 'react';
+import Landing from "./pages/Landing";
+import "./index.css";
+import "./App.css";
+import { Navbar } from "./components/Navbar";
+import { Faq } from "./pages/Faq";
+import { Loader } from "./components/Loader/Loader";
+import { useEffect, useState } from "react";
 
 // import {black} from '@mui/material/colors';
 
 function App() {
-  const ref = useRef()
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener("load", () => {
+      setLoading(false);
+    });
+  });
+
   return (
     <>
-      <Navbar />
-      <section
-        id="landing" 
-      >
-        <Landing />
-      </section>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Navbar />
+          <section id="landing">
+            <Landing />
+          </section>
 
-        <section 
-          id="faq" 
-        >
-          <Faq />
-        </section>
+          <section id="faq">
+            <Faq />
+          </section>
+        </>
+      )}
     </>
   );
 }
