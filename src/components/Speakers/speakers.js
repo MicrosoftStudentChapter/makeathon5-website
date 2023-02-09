a0ef8cb05f31fb7620c0fd0ad6119ef2886b0836
Vishist Bhagabati <55060840+VishistB@users.noreply.github.com>
2023-02-09 21:25:58 +0530

    added speaker pictures

--- src/components/Speakers/speakers.js
+++ src/components/Speakers/speakers.js
@@ -19,157 +19,157 @@ function Speakers() {
     const [card3, setCard3] = useState(0);
     const [card4, setCard4] = useState(0);
     const [card5, setCard5] = useState(0);
-    
+
     const [disp1, setDisp1] = useState("none");
     const [disp2, setDisp2] = useState("none");
     const [disp3, setDisp3] = useState("none");
     const [disp4, setDisp4] = useState("none");
     const [disp5, setDisp5] = useState("none");
-    
+
     function shownone() {
-        console.log('hi1')
-        setIndex1(1)
-        setIndex2(1)
-        setIndex3(1)
-        setIndex4(1)
-        setIndex5(1)
-        setCard1(0)
-        setCard2(0)
-        setCard3(0)
-        setCard4(0)
-        setCard5(0)
-        setDisp1("none")
-        setDisp2("none")
-        setDisp3("none")
-        setDisp4("none")
-        setDisp5("none")
+        console.log("hi1");
+        setIndex1(1);
+        setIndex2(1);
+        setIndex3(1);
+        setIndex4(1);
+        setIndex5(1);
+        setCard1(0);
+        setCard2(0);
+        setCard3(0);
+        setCard4(0);
+        setCard5(0);
+        setDisp1("none");
+        setDisp2("none");
+        setDisp3("none");
+        setDisp4("none");
+        setDisp5("none");
     }
     function showone() {
-        console.log('hi1')
-        setIndex1(1)
-        setIndex2(0)
-        setIndex3(0)
-        setIndex4(0)
-        setIndex5(0)
-        setCard1(1)
-        setCard2(0)
-        setCard3(0)
-        setCard4(0)
-        setCard5(0)
-        setDisp1("")
-        setDisp2("none")
-        setDisp3("none")
-        setDisp4("none")
-        setDisp5("none")
+        console.log("hi1");
+        setIndex1(1);
+        setIndex2(0);
+        setIndex3(0);
+        setIndex4(0);
+        setIndex5(0);
+        setCard1(1);
+        setCard2(0);
+        setCard3(0);
+        setCard4(0);
+        setCard5(0);
+        setDisp1("");
+        setDisp2("none");
+        setDisp3("none");
+        setDisp4("none");
+        setDisp5("none");
     }
     function showtwo() {
-        console.log('hi2')
-        setIndex1(0)
-        setIndex2(1)
-        setIndex3(0)
-        setIndex4(0)
-        setIndex5(0)
-        setCard1(0)
-        setCard2(1)
-        setCard3(0)
-        setCard4(0)
-        setCard5(0)
-        setDisp1("none")
-        setDisp2("")
-        setDisp3("none")
-        setDisp4("none")
-        setDisp5("none")
+        console.log("hi2");
+        setIndex1(0);
+        setIndex2(1);
+        setIndex3(0);
+        setIndex4(0);
+        setIndex5(0);
+        setCard1(0);
+        setCard2(1);
+        setCard3(0);
+        setCard4(0);
+        setCard5(0);
+        setDisp1("none");
+        setDisp2("");
+        setDisp3("none");
+        setDisp4("none");
+        setDisp5("none");
     }
     function showthree() {
-        console.log('hi3')
-        setIndex1(0)
-        setIndex2(0)
-        setIndex3(1)
-        setIndex4(0)
-        setIndex5(0)
-        setCard1(0)
-        setCard2(0)
-        setCard3(1)
-        setCard4(0)
-        setCard5(0)
-        setDisp1("none")
-        setDisp2("none")
-        setDisp3("")
-        setDisp4("none")
-        setDisp5("none")
+        console.log("hi3");
+        setIndex1(0);
+        setIndex2(0);
+        setIndex3(1);
+        setIndex4(0);
+        setIndex5(0);
+        setCard1(0);
+        setCard2(0);
+        setCard3(1);
+        setCard4(0);
+        setCard5(0);
+        setDisp1("none");
+        setDisp2("none");
+        setDisp3("");
+        setDisp4("none");
+        setDisp5("none");
     }
     function showfour() {
-        console.log('hi4')
-        setIndex1(0)
-        setIndex2(0)
-        setIndex3(0)
-        setIndex4(1)
-        setIndex5(0)
-        setCard1(0)
-        setCard2(0)
-        setCard3(0)
-        setCard4(1)
-        setCard5(0)
-        setDisp1("none")
-        setDisp2("none")
-        setDisp3("none")
-        setDisp4("")
-        setDisp5("none")
+        console.log("hi4");
+        setIndex1(0);
+        setIndex2(0);
+        setIndex3(0);
+        setIndex4(1);
+        setIndex5(0);
+        setCard1(0);
+        setCard2(0);
+        setCard3(0);
+        setCard4(1);
+        setCard5(0);
+        setDisp1("none");
+        setDisp2("none");
+        setDisp3("none");
+        setDisp4("");
+        setDisp5("none");
     }
     function showfive() {
-        console.log('hi5')
-        setIndex1(0)
-        setIndex2(0)
-        setIndex3(0)
-        setIndex4(0)
-        setIndex5(1)
-        setCard1(0)
-        setCard2(0)
-        setCard3(0)
-        setCard4(0)
-        setCard5(1)
-        setDisp1("none")
-        setDisp2("none")
-        setDisp3("none")
-        setDisp4("none")
-        setDisp5("")
+        console.log("hi5");
+        setIndex1(0);
+        setIndex2(0);
+        setIndex3(0);
+        setIndex4(0);
+        setIndex5(1);
+        setCard1(0);
+        setCard2(0);
+        setCard3(0);
+        setCard4(0);
+        setCard5(1);
+        setDisp1("none");
+        setDisp2("none");
+        setDisp3("none");
+        setDisp4("none");
+        setDisp5("");
     }
     const cardivstyle1 = {
-        transform: "Translate(0,0%)",
-        zIndex:2,
+        transform: "Translate(0,-25%)",
+        zIndex: 2,
         display: "flex",
         width: "100%",
         transition: "all 0.5s",
     };
     const cardivstyle2 = {
-        transform: "Translate(0,25%)",
-        zIndex:3,
+        transform: "Translate(0,0%)",
+        zIndex: 3,
         display: "flex",
         width: "100%",
         transition: "all 0.5s",
     };
     const cardivstyle3 = {
-        transform: "Translate(0,50%)",
-        zIndex:4,
+        transform: "Translate(0,25%)",
+        zIndex: 4,
         display: "flex",
         width: "100%",
         transition: "all 0.5s",
     };
     const cardivstyle4 = {
-        transform: "Translate(0,25%)",
-        zIndex:3,
+        transform: "Translate(0,0%)",
+        zIndex: 3,
         display: "flex",
         width: "100%",
         transition: "all 0.5s",
     };
     const cardivstyle5 = {
-        transform: "Translate(0,0%)",
-        zIndex:2,
+        transform: "Translate(0,-25%)",
+        zIndex: 2,
         display: "flex",
         width: "100%",
         transition: "all 0.5s",
     };
-    
+
     return (
         <div className={styles.speakerspage}>
             <div className={styles.speakerspagewrapper}>
@@ -177,32 +177,75 @@ function Speakers() {
                 {/* <div className={styles.speakercard}>
 
                 </div> */}
+                <div className={styles.cardflex}>
                 <div className={styles.cardwrap}>
-                    <div style={cardivstyle1} className={styles.carddiv} onMouseOver={showone} onMouseLeave={shownone}>
-                        <SpeakerCard value="0%" zvalue="2" opaque={index1} />
-                        <Speakerdesc see={card1} move="61%" present={disp1}/>
+                    <div
+                        style={cardivstyle1}
+                        className={styles.carddiv}
+                        onMouseOver={showone}
+                        onMouseLeave={shownone}
+                    >
+                        <SpeakerCard
+                            value="0%"
+                            zvalue="2"
+                            opaque={index1}
+                            source="https://res.cloudinary.com/damjdcgak/image/upload/v1675794811/Makeathon%205%20website/Speakers/WhatsApp_Image_2023-02-07_at_23.43.42_fnbwok-removebg-preview_kneizb.png"
+                        />
+                        <Speakerdesc see={card1} move="61%" present={disp1} />
                     </div>
-                    <div style={cardivstyle2} className={styles.carddiv} onMouseOver={showtwo} onMouseLeave={shownone}>
-                        <SpeakerCard value="0%" zvalue="2" opaque={index2} />
-                        <Speakerdesc see={card2} move="61%" present={disp2}/>
+                    <div
+                        style={cardivstyle2}
+                        className={styles.carddiv}
+                        onMouseOver={showtwo}
+                        onMouseLeave={shownone}
+                    >
+                        <SpeakerCard
+                            value="0%"
+                            zvalue="2"
+                            opaque={index2}
+                            source="https://res.cloudinary.com/damjdcgak/image/upload/v1675797998/Makeathon%205%20website/Speakers/WhatsApp_Image_2023-02-07_at_23.42.33_kb1xs7-removebg-preview_wtiskc.png"
+                        />
+                        <Speakerdesc see={card2} move="61%" present={disp2} />
                     </div>
 
-                    <div style={cardivstyle3} className={styles.carddiv} onMouseOver={showthree} onMouseLeave={shownone}>
-                        <SpeakerCard value="0%" zvalue="2" opaque={index3} />
-                        <Speakerdesc see={card3} move="61%" present={disp3}/>
+                    <div
+                        style={cardivstyle3}
+                        className={styles.carddiv}
+                        onMouseOver={showthree}
+                        onMouseLeave={shownone}
+                    >
+                        <SpeakerCard
+                            value="0%"
+                            zvalue="2"
+                            opaque={index3}
+                            source="https://res.cloudinary.com/damjdcgak/image/upload/v1675794854/Makeathon%205%20website/Speakers/WhatsApp_Image_2023-02-07_at_23.44.47_g9pcfz-removebg-preview_vpmhjn.png"
+                        />
+                        <Speakerdesc see={card3} move="61%" present={disp3} />
                     </div>
-                    <div style={cardivstyle4} className={styles.carddiv} onMouseOver={showfour} onMouseLeave={shownone}>
-                        <SpeakerCard value="0%" zvalue="2" opaque={index4} />
-                        <Speakerdesc see={card4} move="-105%" present={disp4}/>
+                    <div
+                        style={cardivstyle4}
+                        className={styles.carddiv}
+                        onMouseOver={showfour}
+                        onMouseLeave={shownone}
+                    >
+                        <SpeakerCard
+                            value="0%"
+                            zvalue="2"
+                            opaque={index4}
+                            source="https://res.cloudinary.com/damjdcgak/image/upload/v1675797928/Makeathon%205%20website/Speakers/IMG_4351-removebg-preview_dhi8pg.png"
+                        />
+                        <Speakerdesc see={card4} move="-105%" present={disp4} />
                     </div>
-                    <div style={cardivstyle5} className={styles.carddiv} onMouseOver={showfive} onMouseLeave={shownone}>
-                        <SpeakerCard value="0%" zvalue="2" opaque={index5} />   
-                        <Speakerdesc see={card5} move="-105%" present={disp5}/>
+                    <div
+                        style={cardivstyle5}
+                        className={styles.carddiv}
+                        onMouseOver={showfive}
+                        onMouseLeave={shownone}
+                    >
+                        <SpeakerCard value="0%" zvalue="2" opaque={index5} />
+                        <Speakerdesc see={card5} move="-105%" present={disp5} />
                     </div>
-                    
-                    
-                    
-                    
+                </div>
                 </div>
             </div>
         </div>
