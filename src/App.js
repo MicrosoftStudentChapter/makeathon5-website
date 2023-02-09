1315a419ac1dedce3fb746562eabc71834e3628c
destrex271 <destrex271@gmail.com>
2023-02-10 02:52:09 +0530

    conflivt resolved

--- src/App.js
+++ src/App.js
@@ -32,7 +32,6 @@ function App() {
           <section id="landing">
             <Landing />
           </section>
-<<<<<<< HEAD
           <section id="timeline">
             {/* <div className="head"> */}
             {/*   Timeline */}
@@ -42,11 +41,9 @@ function App() {
           {/* <section id="speakers"> */}
           {/*   <Speakers /> */}
           {/* </section> */}
-=======
           <section id="tracks">
             <Tracks />
           </section>
->>>>>>> develop
           <section id="faq">
             <Faq />
           </section>
