0a47f8a2da8cf9533486af419e084c6329857a66
nimishmedatwal <medatwalnimish@gmail.com>
2023-02-22 23:38:03 +0530

    barbad theek ho gya

--- src/pages/Prizes/index.js
+++ src/pages/Prizes/index.js
@@ -1,12 +1,13 @@
 import styled from "@emotion/styled";
 import { Box, Typography } from "@mui/material";
-
+import data from "./data.json";
 import styles from "./prizes.module.css";
 const ParentBox = styled(Box)({
   width: "100vw",
   height: "100vh",
   display: "flex",
   flexDirection: "column",
+  backgroundColor: "black",
 });
 
 const TitleText = styled(Typography)({
@@ -16,7 +17,7 @@ const TitleText = styled(Typography)({
   color: "white",
 });
 
-const Prizecard1 = styled(Box)({
+const PrizeCard = styled(Box)({
   width: "30vw",
   height: "30vh",
   backgroundColor: "#ffffff",
@@ -25,7 +26,7 @@ const Prizecard1 = styled(Box)({
 
 export const Prizes = () => {
   return (
-    <ParentBox className={styles.prize}>
+    <ParentBox>
       <Box
         sx={{
           display: "flex",
@@ -37,128 +38,131 @@ export const Prizes = () => {
         }}
       >
         <TitleText variant="h1">Prizes</TitleText>
-        
-        <div>
-          <a className={styles.card} href="#!">
-            <div
-              className={styles.front}
-              style={{
-                backgroundImage: "url(//source.unsplash.com/300x401)",
-              }}
-            >
-              <p>Lorem ipsum dolor sit amet consectetur adipisi.</p>
-            </div>
-            <div className={styles.back}>
-              <div>
-                <p>Consectetur adipisicing elit. Possimus, praesentium?</p>
-                <p>
-                  Provident consectetur natus voluptatem quis tenetur sed beatae
-                  eius sint.
-                </p>
-                <button className={styles.button}>Click Here</button>
+        <div className={styles.cards}>
+          <div className={styles.card}>
+            <span className={styles.close} />
+            <span className={styles.arrow} />
+            <article>
+              <h2>Best Hack</h2>
+              <div className={styles.title}>Bug Collector</div>
+              <div className={styles.pic}>
+                <img src="https://rafaelavlucas.github.io/assets/images/img-1.jpeg" />
               </div>
-            </div>
-          </a>
-          <a className={styles.card} href="#!">
-            <div
-              className={styles.front}
-              style={{
-                backgroundImage: "url(//source.unsplash.com/300x402)",
-              }}
-            >
-              <p>Lorem ipsum dolor sit amet consectetur adipisi.</p>
-            </div>
-            <div className={styles.back}>
-              <div>
-                <p>Consectetur adipisicing elit. Possimus, praesentium?</p>
-                <p>
-                  Provident consectetur natus voluptatem quis tenetur sed beatae
-                  eius sint.
-                </p>
-                <button className={styles.button}>Click Here</button>
+              <div className={styles.desc}>
+                Elijah has collected bugs since they were six years old and now
+                has many dozen bugs but none in their pants.
               </div>
+            </article>
+            <div className={styles.actions}>
+              <button className={styles.btn}>
+                <span>like</span>
+                <img
+                  className={styles.icon}
+                  src="https://rafaelavlucas.github.io/assets/icons/misc/heart.svg"
+                />
+              </button>
+              <button className={styles.btn}>
+                <span>trade</span>
+                <img
+                  className={styles.icon}
+                  src="https://rafaelavlucas.github.io/assets/icons/misc/trade.svg"
+                />
+              </button>
             </div>
-          </a>
-          <a className={styles.card} href="#!">
-            <div
-              className={styles.front}
-              style={{
-                backgroundImage: "url(//source.unsplash.com/300x403)",
-              }}
-            >
-              <p>Lorem ipsum dolor sit amet consectetur adipisi.</p>
-            </div>
-            <div className={styles.back}>
-              <div>
-                <p>Consectetur adipisicing elit. Possimus, praesentium?</p>
-                <p>
-                  Provident consectetur natus voluptatem quis tenetur sed beatae
-                  eius sint.
-                </p>
-                <button className={styles.button}>Click Here</button>
+          </div>
+          <div className={styles.card}>
+            <span className={styles.close} />
+            <span className={styles.arrow} />
+            <article>
+              <h2>@nd Best Hack</h2>
+              <div className={styles.title}>Ant Collector</div>
+              <div className={styles.pic}>
+                <img src="https://images.unsplash.com/photo-1543852786-1cf6624b9987?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80" />
               </div>
-            </div>
-          </a>
-          <a className={styles.card} href="#!">
-            <div
-              className={styles.front}
-              style={{
-                backgroundImage: "url(//source.unsplash.com/300x404)",
-              }}
-            >
-              <p>Lorem ipsum dolor sit amet consectetur adipisi.</p>
-            </div>
-            <div className={styles.back}>
-              <div>
-                <p>Consectetur adipisicing elit. Possimus, praesentium?</p>
-                <p>
-                  Provident consectetur natus voluptatem quis tenetur sed beatae
-                  eius sint.
-                </p>
-                <button className={styles.button}>Click Here</button>
+              <div className={styles.desc}>
+                Morgan has collected ants since they were six years old and now
+                has many dozen ants but none in their pants.
               </div>
+            </article>
+            <div className={styles.actions}>
+              <button className={styles.btn}>
+                <span>like</span>
+                <img
+                  className={styles.icon}
+                  src="https://rafaelavlucas.github.io/assets/icons/misc/heart.svg"
+                />
+              </button>
+              <button className={styles.btn}>
+                <span>trade</span>
+                <img
+                  className={styles.icon}
+                  src="https://rafaelavlucas.github.io/assets/icons/misc/trade.svg"
+                />
+              </button>
             </div>
-          </a>
-          <a className={styles.card} href="#!">
-            <div
-              className={styles.front}
-              style={{
-                backgroundImage: "url(//source.unsplash.com/300x405)",
-              }}
-            >
-              <p>Lorem ipsum dolor sit amet consectetur adipisi.</p>
-            </div>
-            <div className={styles.back}>
-              <div>
-                <p>Consectetur adipisicing elit. Possimus, praesentium?</p>
-                <p>
-                  Provident consectetur natus voluptatem quis tenetur sed beatae
-                  eius sint.
-                </p>
-                <button className={styles.button}>Click Here</button>
+          </div>
+          <div className={styles.card}>
+            <span className={styles.close} />
+            <span className={styles.arrow} />
+            <article>
+              <h2>3rd Best Hack</h2>
+              <div className={styles.title}>Fly Collector</div>
+              <div className={styles.pic}>
+                <img src="https://images.unsplash.com/photo-1506755855567-92ff770e8d00?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1268&q=80" />
               </div>
-            </div>
-          </a>
-          <a className={styles.card} href="#!">
-            <div
-              className={styles.front}
-              style={{
-                backgroundImage: "url(//source.unsplash.com/300x406)",
-              }}
-            >
-              <p>Lorem ipsum dolor sit amet consectetur adipisi.</p>
-            </div>
-            <div className={styles.back}>
-              <div>
-                <p>Consectetur adipisicing elit. Possimus, praesentium?</p>
-                <p>
-                  Provident consectetur natus voluptatem quis tenetur sed beatae
-                  eius sint.
-                </p>
-                <button className={styles.button}>Click Here</button>
+              <div className={styles.desc}>
+                Adrian has collected flies since they were six years old and now
+                has many dozen flies but none in their pants.
               </div>
+            </article>
+            <div className={styles.actions}>
+              <button className={styles.btn}>
+                <span>like</span>
+                <img
+                  className={styles.icon}
+                  src="https://rafaelavlucas.github.io/assets/icons/misc/heart.svg"
+                />
+              </button>
+              <button className={styles.btn}>
+                <span>trade</span>
+                <img
+                  className={styles.icon}
+                  src="https://rafaelavlucas.github.io/assets/icons/misc/trade.svg"
+                />
+              </button>
             </div>
-          </a>
+          </div>
+          <div className={styles.card}>
+            <span className={styles.close} />
+            <span className={styles.arrow} />
+            <article>
+              <h2>3rd Best Hack</h2>
+              <div className={styles.title}>Fly Collector</div>
+              <div className={styles.pic}>
+                <img src="https://images.unsplash.com/photo-1506755855567-92ff770e8d00?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1268&q=80" />
+              </div>
+              <div className={styles.desc}>
+                Adrian has collected flies since they were six years old and now
+                has many dozen flies but none in their pants.
+              </div>
+            </article>
+            <div className={styles.actions}>
+              <button className={styles.btn}>
+                <span>like</span>
+                <img
+                  className={styles.icon}
+                  src="https://rafaelavlucas.github.io/assets/icons/misc/heart.svg"
+                />
+              </button>
+              <button className={styles.btn}>
+                <span>trade</span>
+                <img
+                  className={styles.icon}
+                  src="https://rafaelavlucas.github.io/assets/icons/misc/trade.svg"
+                />
+              </button>
+            </div>
+          </div>
         </div>
       </Box>
     </ParentBox>
